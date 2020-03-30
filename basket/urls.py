bcaef0654677789c088006ca70a789c4912ffd04
Jakob Damgaard Møller <jakobdo@gmail.com>
2020-03-30 15:43:43 +0200

    Basket in the making

--- basket/urls.py
+++ basket/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from basket.views import BasketAddView, BasketIndexView
+from basket.views import BasketAddView, BasketIndexView, BacketRemoveView
 
 
 urlpatterns = [
     path('', BasketIndexView.as_view(), name="basket_index"),
-    path('add/', BasketAddView.as_view(), name="add_basket"),
+    path('add/', BasketAddView.as_view(), name="basket_add"),
+    path('remove/<int:product>/', BacketRemoveView.as_view(), name="basket_remove"),
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.shortcuts import redirect, render
+from django.urls import reverse, reverse_lazy
+from django.views import View
 from django.views.generic import FormView, TemplateView
 
 from basket.forms import BasketAddForm
+from product.models import Product, ProductColor, ProductSize
 
 
 class Basket:
@@ -45,5 +47,54 @@ class BasketIndexView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(BasketIndexView, self).get_context_data(**kwargs)
-        context['basket'] = self.request.session.get('basket', [])
+        basket = self.request.session.get('basket', [])
+        
+        # Load products
+        products = {p.id: p for p in Product.objects.filter(pk__in=[item.get('product') for item in basket]).select_related('shop')}
+        # Load sizes
+        sizes = {s.id: s.name for s in ProductSize.objects.filter(pk__in=[item.get('size') for item in basket])}
+        # Load colors
+        colors = {c.id: c.name for c in ProductColor.objects.filter(pk__in=[item.get('color') for item in basket])}
+
+        new_basket = []
+
+        for item in basket:
+            count = item.get('count')
+            price = products.get(item.get('product')).price
+            new_item = dict(
+                product_id=item.get('product'),
+                product=products.get(item.get('product')).name,
+                shop=products.get(item.get('product')).shop.name,
+                size_id=item.get('size'),
+                size=sizes.get(item.get('size')),
+                color_id=item.get('color'),
+                color=colors.get(item.get('color')),
+                count=count,
+                price=price,
+                subtotal=count * price
+            )
+            new_basket.append(new_item)
+        context['basket'] = new_basket
+
         return context
+
+
+class BacketRemoveView(View):
+    def post(self, request, *args, **kwargs):
+        product = kwargs.get('product')
+        try:
+            color = int(request.POST.get('color'))
+        except:
+            color = None
+        try:
+            size = int(request.POST.get('size'))
+        except:
+            size = None
+        basket = self.request.session.get('basket', [])
+        for i in range(len(basket)):
+            if basket[i]['product'] == product and basket[i]['color'] == color and basket[i]['size'] == size:
+                del basket[i] 
+                break
+        self.request.session['basket'] = basket
+        self.request.session.modified = True
+        return redirect(reverse('basket_index'))
