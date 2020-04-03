f9bb77d06573add44d944fa32da0886f39b7b96f
Jakob Damgaard Møller <jakobdo@gmail.com>
2020-04-04 00:40:20 +0200

    Contact form added to get in touch with the shop

--- shop/urls.py
+++ shop/urls.py
@@ -1,11 +1,11 @@
-from django.contrib.auth import views as auth_views
 from django.urls import path
 
 from product.views import IndexView
-from shop.views import ShopsDetailView, ShopsListView
+from shop.views import ShopsDetailView, ShopsListView, ShopContactView
 
 urlpatterns = [
     path('', ShopsListView.as_view(), name="shops"),
-    path('<int:pk>/', ShopsDetailView.as_view(), name="shops_detail"),
+    path('<int:pk>/', ShopsDetailView.as_view(), name="shop_detail"),
     path('<int:pk>/products/', IndexView.as_view(), name="shop_product_list"),
+    path('<int:pk>/contact/', ShopContactView.as_view(), name="shop_contact"),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,12 @@
-from django.views.generic import DetailView, ListView
+from django.contrib.messages.views import SuccessMessageMixin
+from django.core.mail import send_mail
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404
+from django.urls import reverse, reverse_lazy
+from django.utils.translation import gettext_lazy
+from django.views.generic import DetailView, FormView, ListView
 
+from shop.forms import ShopContactForm
 from shop.models import Shop
 
 
@@ -25,3 +32,26 @@ class ShopsDetailView(DetailView):
     def get_queryset(self):
         queryset = self.model.objects.filter(active=True)
         return queryset
+
+
+class ShopContactView(SuccessMessageMixin, FormView):
+    """
+    Contact shop form. Will send an email to the shop contact
+    """
+    template_name = 'shop/contact.html'
+    form_class = ShopContactForm
+    success_message = gettext_lazy("Message sent to shop, they will get in touch.")
+
+    def get_success_url(self):
+        return reverse('shop_detail', kwargs={'pk': self.kwargs.get('pk')})
+
+    def form_valid(self, form):
+        shop = get_object_or_404(Shop, pk=self.kwargs.get('pk'))
+        send_mail(
+            subject=form.cleaned_data.get('subject'),
+            message=form.cleaned_data.get('message'),
+            from_email=form.cleaned_data.get('email'),
+            recipient_list=[shop.email],
+            fail_silently=False,
+        )
+        return super().form_valid(form)
