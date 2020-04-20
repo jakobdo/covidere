a85560a1c256e0571b3f753e9dbc8b2e6f098bb9
Jakob Damgaard Møller <jakobdo@gmail.com>
2020-04-20 15:03:04 +0200

    Password reset and change added

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,19 @@
 from django.urls import path
+from django.contrib.auth import views
 
 from base.views import ActivateUserView
 
+
 urlpatterns = [
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+
     path('activate/<str:uidb64>/<str:token>/', ActivateUserView.as_view(), name="user_activate"),
+    
+    path('password/change/', views.PasswordChangeView.as_view(), name='password_change'),
+    path('password/change/done/', views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    path('password/reset/', views.PasswordResetView.as_view(), name='password_reset'),
+    path('password/reset/done/', views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
