d8e5ec7992941b93271be57418d81a21433f75d9
Jakob Damgaard Møller <jakobdo@gmail.com>
2020-04-21 14:36:22 +0200

    Changes to urls, so should work in dev and prod

--- shoplokalt/urls.py
+++ shoplokalt/urls.py
@@ -14,8 +14,6 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
 from django.urls import include, path
 
 from product.views import ProductsView
@@ -29,11 +27,13 @@ urlpatterns = [
     path('order/', include('order.urls')),
     path('shops/', include('shop.urls')),
     path('shopadmin/', include('shop.adminurls')),
-    path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 if settings.DEBUG:
+    from django.conf.urls.static import static
+    from django.contrib import admin
     import debug_toolbar
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns
+        path('admin/', admin.site.urls),
+    ] + urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
