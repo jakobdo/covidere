3eb0fae0140b782c450a3277b5320c640e4bf9eb
Jakob Damgaard Møller <jakobdo@gmail.com>
2020-04-27 13:46:38 +0200

    Email made unique on user

--- base/models.py
+++ base/models.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.translation import gettext_lazy
 
 
 class User(AbstractUser):
+    email = models.EmailField(gettext_lazy('email address'), unique=True)
+
     class Meta:
         permissions = (
             ("alten_admin", "Can administrate shops in Alten Admin"),
