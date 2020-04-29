13151e6cb877fd7b121c0fc03c3a99d3a2903f79
Mikkel HANSEN <mikkel.hansen@alten.dk>
2020-04-29 16:40:31 +0200

    Modified order/forms.py to use Postcode queryset for active postcodes.

--- order/forms.py
+++ order/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 from order.models import Order
-
+from postcode.models import Postcode
 
 class OrderForm(forms.ModelForm):
     terms = forms.BooleanField(label='Jeg accepterer at jeg handler direkte med butikken, og min bestilling er først endelig når betalingen er gennemført, og butikken har bekræftet.', required=True)
@@ -9,3 +9,7 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ['name', 'address', 'postcode', 'email', 'mobile', 'terms']
+
+    def __init__(self, *args, **kwargs):
+        super(OrderForm, self).__init__(*args, **kwargs)
+        self.fields['postcode'].queryset = Postcode.objects.filter(active=True)
