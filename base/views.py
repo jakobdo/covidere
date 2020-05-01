fbae7cce2fbf9ecc6d5fe867f82ef1451b6b7b98
Jakob Damgaard Møller <jakobdo@gmail.com>
2020-05-01 13:14:53 +0200

    Password validation added and view refactored

--- base/views.py
+++ base/views.py
@@ -17,40 +17,37 @@ class ActivateUserView(FormView):
     template_name = 'base/set_password.html'
     form_class = SetUsernameAndPasswordForm
 
-    def validate_user(self):
+    def validate_token(self):
+        return self.user.is_active is False and account_activation_token.check_token(self.user, self.kwargs.get('token'))
+
+    def get_form_kwargs(self):
+        kwargs = super(ActivateUserView, self).get_form_kwargs()
+        kwargs['user'] = self.user
+        return kwargs
+
+    def form_valid(self, form):
+        user = self.user
+        user.is_active = True
+        user.username = form.cleaned_data["username"]
+        user.set_password(form.cleaned_data["password1"])
+        try:
+            user.save()
+        except IntegrityError:
+            form.add_error('username', gettext('Shop with this username already exists.'))
+            return super(ActivateUserView, self).form_invalid(form)
+
+        login(self.request, user, backend='axes.backends.AxesBackend')
+        return super().form_valid(form)
+
+    def __init__(self, **kwargs):
         try:
             uid = force_text(urlsafe_base64_decode(self.kwargs.get('uidb64')))
             self.user = User.objects.get(pk=uid)
         except (TypeError, ValueError, OverflowError, User.DoesNotExist):
-            self.user = None
-    
-    def validate_token(self):
-        return self.user is not None and self.user.is_active is False and account_activation_token.check_token(self.user, self.kwargs.get('token'))
-
-    def form_valid(self, form):
-        self.validate_user()
-
-        if self.validate_token():
-            user = self.user
-            user.is_active = True
-            user.username = form.cleaned_data["username"]
-            user.set_password(form.cleaned_data["password1"])
-            try:
-                user.save()
-            except IntegrityError:
-                form.add_error('username', gettext('Shop with this username already exists.'))
-                return super(ActivateUserView, self).form_invalid(form)
-
-            login(self.request, user, backend='axes.backends.AxesBackend')
-        else:
             raise Http404(gettext("Invalid token"))
-        return super().form_valid(form)
-
-    def get(self, request, *args, **kwargs):
-        self.validate_user()
         if not self.validate_token():
             raise Http404(gettext("Invalid token"))
-        return super().get(request, *args, **kwargs)
+        super().__init(**kwargs)
 
     def get_success_url(self):
         return reverse('shop_overview')
