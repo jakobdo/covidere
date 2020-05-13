57bff1df88ec205653903673942e9573771d6115
Mikkel HANSEN <mikkel.hansen@alten.dk>
2020-05-13 14:11:42 +0200

    Added pytest-django to requirements, added pytest.ini and first url test.

--- shop/tests.py
+++ shop/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse, resolve
 
-# Create your tests here.
+class TestUrls:
+
+    def test_shops_url_resolves(self):
+        path = reverse('shops')
+
+        assert resolve(path).view_name == 'shop'
