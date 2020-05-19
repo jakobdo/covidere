23e3b5cdd1bc885c8afc08ccc03af0d2f1f5bb23
oktayalizada <oktay.alizada@gmail.com>
2020-05-19 23:02:50 +0200

    Allowed hosts

--- shoplokalt/settings.py
+++ shoplokalt/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'secret')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '*']
 
 # Application definition
 
