5478c0048ebf2b0cb9b365fcf947378c369d6b1d
Jakob Damgaard Møller <jakobdo@gmail.com>
2020-05-26 10:43:10 +0200

    github workflow postgres settings changed

--- shoplokalt/settings.py
+++ shoplokalt/settings.py
@@ -119,6 +119,18 @@ DATABASES = {
     }
 }
 
+if os.environ.get('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.environ.get('POSTGRES_DB'),
+            'USER': os.environ.get('POSTGRES_USER'),
+            'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+            'HOST': '127.0.0.1',
+            'PORT': '5432',
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
