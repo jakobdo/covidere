a72cd9381e01560c50f4facc161fc6d797d142da
Jakob Damgaard Møller <jakobdo@gmail.com>
2020-05-26 14:22:32 +0200

    Fixed tests

--- tests/shop/test_forms.py
+++ tests/shop/test_forms.py
@@ -31,18 +31,20 @@ class TestShopRegisterForm:
     @pytest.mark.parametrize(
         'cvr_number, name, address, email, phone, postcode_special, city_special, result',
         [
-            (None, 'Name', 'Address 123', 'user@email.tld', '43211234', '1234', 'City', False),
-            ('12345678', None, 'Address 123', 'user@email.tld', '43211234', '1234', 'City', False),
-            ('12345678', 'Name', None, 'user@email.tld', '43211234', '1234', 'City', False),
-            ('12345678', 'Name', 'Address 123', 'user@email.tld',  None, '1234', 'City', False),
-            ('12345678', 'Name', 'Address 123', 'user@email.tld', '43211234', None, 'City', False),
-            ('12345678', 'Name', 'Address 123', 'user@email.tld', '43211234', '1234', None, False),
+            (None, 'Name', 'Address 123', 'user1@email.tld', '43211234', '1234', 'City', False),
+            ('12345678', None, 'Address 123', 'user2@email.tld', '43211234', '1234', 'City', False),
+            ('12345678', 'Name', None, 'user3@email.tld', '43211234', '1234', 'City', False),
+            ('12345678', 'Name', 'Address 123', 'user4@email.tld',  None, '1234', 'City', False),
+            ('12345678', 'Name', 'Address 123', 'user5@email.tld', '43211234', None, 'City', False),
+            ('12345678', 'Name', 'Address 123', 'user6@email.tld', '43211234', '1234', None, False),
             ('12345678', 'Name', 'Address 123', 'fake-email', '43211234', '1234', 'City', False),
-            ('12345678', 'Name', 'Address 123', 'user@email.tld', '43211234', '1234', 'City', True),
+            ('12345678', 'Name', 'Address 123', 'user7@email.tld', '43211234', '1234', 'City', True),
         ]
     )
     def test_shop_register__form(
         self,
+        db,
+        postcode,
         cvr_number,
         name,
         address,
@@ -61,4 +63,6 @@ class TestShopRegisterForm:
             'postcode_special': postcode_special,
             'city_special': city_special
         }
-        assert self.form(data=data).is_valid() == result
+        form = self.form(data=data)
+        form_result = form.is_valid()
+        assert form_result == result
